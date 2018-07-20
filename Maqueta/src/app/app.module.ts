84e5916883e2a19c44063c87bd17c4b7c41cf12f
Cristian Valenzuela <cristianbdh1@gmail.com>
2018-07-20 18:50:01 -0400

    maquetina
    maqueta Bootstrap / Jquery / Iconos / astr

---
+++ Maqueta/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {HttpModule} from '@angular/http';
+import {ToastrModule} from 'ngx-toastr';
+
+import { AppComponent } from './app.component';
+import { routing, appRoutingProviders } from './app.routing';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    HttpModule,
+    routing,
+    ToastrModule.forRoot()
+  ],
+  providers: [
+    appRoutingProviders
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Maqueta/src/app/app.routing.ts
@@ -0,0 +1,11 @@
+import { AppComponent } from './app.component';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+
+const appRoutes: Routes = [
+    
+];
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
